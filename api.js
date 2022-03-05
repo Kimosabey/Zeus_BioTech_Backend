21dbad473fd48100f6ea337239ab8bf357784128
kimosabey <harshan.aiyappa@gmail.com>
2022-03-05 16:52:31 +0530

    no need

--- api.js
+++ api.js
@@ -78,37 +78,10 @@ router.all("*", function (req, res, next) {
   next();
 });
 
-// let setCache = function (req, res, next) {
-//   // here you can define period in second, this one is 5 minutes
-//   const period = 60 * 5;
 
-//   // you only want to cache for GET requests
-//   if (req.method == "GET") {
-//     res.set("Cache-control", `public, max-age=${period}`);
-//   } else {
-//     // for the other requests set strict no caching parameters
-//     res.set("Cache-control", `no-store`);
-//   }
-
-//   // remember to call next() to pass on the request
-//   next();
-// };
-
-// now call the new middleware function in your app
-
-// app.use(setCache);
 
 app.use("/api", router);
 
-// // allow cross-origin requests
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 // file Upload -----------------------
 global.__basedir = __dirname;
